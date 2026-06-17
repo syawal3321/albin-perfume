adb90f3388718c2fb6c4b2ad930ef9b497ef2c20
syawal3321 <albinsyawal54@gmail.com>
2026-06-17 22:01:42 +0800

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const hamburger =
+document.querySelector(".hamburger");
+
+const navMenu =
+document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click",()=>{
+
+    navMenu.classList.toggle("active");
+
+});
